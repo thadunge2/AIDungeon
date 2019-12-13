f080aaf3295721fd51939d7f04b0dacdfad35bb7
thadunge2 <58797864+thadunge2@users.noreply.github.com>
2019-12-13 16:01:09 -0500

    Switching to the vanilla GPT2 model

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${0}")"
 BASE_DIR="$(pwd)"
 
-MODELS_DIRECTORY=generator/gpt2/models
+MODELS_DIRECTORY=generator/gpt2#/models
 MODEL_VERSION=model_v5
 MODEL_NAME=model-550
 MODEL_TORRENT_URL="https://github.com/AIDungeon/AIDungeon/files/3935881/model_v5.torrent.zip"
@@ -16,23 +16,25 @@ else
     apt-get install aria2 unzip > /dev/null
     
     echo "Downloading AIDungeon2 Model... (this may take a very, very long time)"
-    mkdir -p "${MODELS_DIRECTORY}"
+    #mkdir -p "${MODELS_DIRECTORY}"
     cd "${MODELS_DIRECTORY}"
-    mkdir "${MODEL_VERSION}"
-    wget "${MODEL_TORRENT_URL}"
-    unzip "${MODEL_TORRENT_BASENAME}"
+    python download_model.py 1558M
+    mv models/1558M models/model_v5
+    #mkdir "${MODEL_VERSION}"
+    #wget "${MODEL_TORRENT_URL}"
+    #unzip "${MODEL_TORRENT_BASENAME}"
     echo -e "\n\n==========================================="
     echo "We are now starting to download the model."
     echo "It will take a while to get up to speed."
     echo "DHT errors are normal."
     echo -e "===========================================\n"
-    aria2c \
-        --max-connection-per-server 16 \
-        --split 64 \
-        --bt-max-peers 500 \
-        --seed-time=0 \
-        --summary-interval=15 \
-        --disable-ipv6 \
-        "${MODEL_TORRENT_BASENAME%.*}"
+    #aria2c \
+    #    --max-connection-per-server 16 \
+    #    --split 64 \
+    #    --bt-max-peers 500 \
+    #    --seed-time=0 \
+    #    --summary-interval=15 \
+    #    --disable-ipv6 \
+    #    "${MODEL_TORRENT_BASENAME%.*}"
     echo "Download Complete!"
 fi
