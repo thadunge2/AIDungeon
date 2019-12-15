b570380da543b33ae0878269f3c4d4e3823481c1
Mark Taiwan <mark.taiwan@gmail.com>
2019-12-15 16:24:46 +0800

    Re-add playsound

--- play.py
+++ play.py
@@ -382,6 +382,8 @@ def play_aidungeon_2():
                             console_print(story_manager.story.results[-1])
                         except FunctionTimedOut:
                             console_print("That input caused the model to hang (timeout is {}, use infto ## command to change)".format(story_manager.inference_timeout))
+                            if ping:
+                                playsound('ping.mp3')
                     except NameError:
                         pass
                     if ping:
