39070738365380a1484bb209aa7b8fdccc9fc359
James Noeckel <jamesn8@cs.washington.edu>
2019-12-26 22:38:16 -0800

    stopgap fix for cut_down_prompt in raw mode

--- generator/gpt2/gpt2_generator.py
+++ generator/gpt2/gpt2_generator.py
@@ -126,11 +126,22 @@ class GPT2Generator:
             return self.generate(prompt, depth=depth+1)
 
         return result
-        
+
     def cut_down_prompt(self, prompt):
-        split_prompt = prompt.split(">")
-        expendable_text = ">".join(split_prompt[2:])
-        return split_prompt[0] + (">" + expendable_text if len(expendable_text) > 0 else "")
+        if not self.raw:
+            split_prompt = prompt.split(">")
+            expendable_text = ">".join(split_prompt[2:])
+            return split_prompt[0] + (">" + expendable_text if len(expendable_text) > 0 else "")
+        else:
+            sentences = string_to_sentence_list(prompt.lstrip())
+            sentences = sentences[1:]
+            new_text = ""
+            for i in range(len(sentences)):
+                if sentences[i] == "<break>":
+                    new_text = new_text + "\n"
+                else:
+                    new_text = new_text + " " + sentences[i]
+            return new_text.lstrip()
         
     def gen_output(self):
         models_dir = os.path.expanduser(os.path.expandvars(self.model_dir))
